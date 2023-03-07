17ca018ae3b9476ba46e6857f9b59bc2775e5631
bahugo <barsottihugo@gmail.com>
2023-03-07 12:41:57 +0100

    Changed bin link

--- lua/mason-registry/index/python-lsp-ruff/init.lua
+++ lua/mason-registry/index/python-lsp-ruff/init.lua
@@ -4,10 +4,11 @@ local pip3 = require "mason-core.managers.pip3"
 return Pkg.new {
     name = "python-lsp-ruff",
     desc = [[Linter plugin for pylsp based using ruff. Formatting via ruff's.
-    (Allows to use ruff in pylsp)]],
+    (Allows to use ruff in pylsp)
+    WARNING : python-lsp-server must be uninstalled before]],
     homepage = "https://github.com/python-lsp/python-lsp-ruff",
     languages = { Pkg.Lang.Python },
     categories = { Pkg.Cat.LSP },
-    install = pip3.packages { "python-lsp-ruff", bin = { "pyslp_ruff" } },
+    install = pip3.packages { "python-lsp-ruff", bin = { "pyslp" } },
 
 }
